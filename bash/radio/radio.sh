2dd5d15fd50067552981b3fa2a6fb1c0f883f3c2
nikita <nvnemkov@gmail.com>
2020-10-27 22:31:01 +0700

    Добавил в скрипт radio.sh вывод списка в уведомления

--- bash/radio/radio.sh
+++ bash/radio/radio.sh
@@ -8,15 +8,25 @@ hotkeys=(
 )
 
 if [[ $1 =~ $num_re ]]; then
-	stations_num=$(wc -l $LISTPATH | grep -Eo "[0-9]+ ")
-	if [[ $1 -gt $stations_num ]] || [[ $1 -eq 0 ]]; then
+
+	stantions_num=$(wc -l $LISTPATH | grep -Eo "[0-9]+");
+
+	if [[ $1 -gt $stantions_num ]] || [[ $1 -eq 0 ]]; then
 			exit 0;
 	fi
 
-	url=$(head -$1 $LISTPATH| tail -1 | grep -Eo ".+  ")
-	cvlc --verbose 0 ${hotkeys[*]} $url
-elif [[ $1 == "list" ]]; then
-		cat $LISTPATH | grep -Eon "  .+";
+	url=$(head -$1 $LISTPATH| tail -1 | grep -Eo ".+  ");
+	cvlc --verbose 0 ${hotkeys[*]} $url;
+
+elif [[ $1 == "list" ]] || [[ $1 == "nlist" ]]; then
+
+		list_stations=$(cat $LISTPATH | grep -Eon "  .+");
+
+		if [[ $1 == "list" ]]; then
+				echo "$list_stations";
+		else
+				notify-send "Список станций" "$list_stations";
+		fi
 else
 		exit 0;	
 fi
